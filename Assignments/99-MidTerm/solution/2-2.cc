dbf6df5b1aa7ff0dcb433b2b176b3bdece394ed7
Marius Herget <info@herget.design>
2018-07-04 19:27:00 +0200

    added new example - needed to change to values to const

--- Assignments/99-MidTerm/solution/2-2.cc
+++ Assignments/99-MidTerm/solution/2-2.cc
@@ -28,7 +28,7 @@ int main() {
 
   // Iterators on elements in a chunk:
   uint16_t first_chunk_elem = *first_chunk.begin();
-  // uint16_t first_chunk_elem2 = *((*first_chunk).begin());
+  // uint16_t first_chunk_elem2 = *(*first_chunk).begin();
   std::cout << first_chunk_elem << '\n';
   // std::cout << first_chunk_elem2 << '\n';
   uint16_t third_chunk_elem = first_chunk[2];
@@ -37,8 +37,22 @@ int main() {
   // // Pointer to data in second chunk:
   auto chunk_1_data = v_chunks[1].data();
   std::cout << *chunk_1_data << '\n';
-  // // Pointer to data in third chunk (= end pointer of data in second chunk):
+  // // Pointer to data in third chunk (= end pointer of data in secondchunk):
   auto chunk_2_data = v_chunks[2].data();
   std::cout << *chunk_2_data << '\n';
+
+  // Iterate chunks:
+  auto first_chunk2 = v_chunks.begin();
+  auto num_chunks2 = std::distance(v_chunks.begin(), v_chunks.end());
+  // --> 128/(16/8) = 64
+
+  // Iterators on elements in a chunk:
+  std::uint16_t first_chunk_elem2 = *first_chunk.begin();
+  std::uint16_t third_chunk_elem2 = first_chunk[2];
+
+  // Pointer to data in second chunk:
+  const std::uint16_t *chunk_1_data2 = v_chunks[1].data();
+  // Pointer to data in third chunk (= end pointer of data in second chunk):
+  const std::uint16_t *chunk_2_data2 = v_chunks[2].data();
   return 1;
 }
